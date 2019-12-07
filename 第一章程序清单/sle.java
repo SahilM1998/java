3195e1f754d7bd469099300163558ce24f285df3
Sylvia-Yuju <46679831+Sylvia-Yuju@users.noreply.github.com>
2019-12-07 13:59:39 +0800

    Add files via upload

---
+++ 第一章程序清单/sle.java
@@ -0,0 +1,11 @@
+package ≥Ã–Ú«Âµ•1;
+
+public class sle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Celsius 35 is Fahrenheit degree");
+		System.out.println((9.0/5)*35+32);
+	}
+
+}
---
+++ 第一章程序清单/wwtm.java
@@ -0,0 +1,12 @@
+package ≥Ã–Ú«Âµ•1;
+
+public class wwtm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Programming is fun!");
+        System.out.println("Fundmantals First");
+        System.out.println("Problem Driven");
+	}
+
+}
